2414e6d063fe1fd61a0bad48937ae0233aaf2cf6
kobus <kobus@cs.arizona.edu>
2020-06-11 18:38:56 -0700

    Added a comment.

--- compties/grep.sh
+++ compties/grep.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# Kobus says. I do not know who wrote this or when. 
+
 # A helper script to find TIES-related things are
 # Usage: ./grep.sh <string>
 # Make sure to enclose the string with quotes if the string has spaces in it.
