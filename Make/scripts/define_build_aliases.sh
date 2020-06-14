8ac564f28c262c32e5c7f97ee0936f1d47e635de
ivilab-sync <sync@ivilab.org>
2020-06-14 06:11:10 -0700

    r25496 | kobus | 2020-06-14 06:11:00 -0700 (Sun, 14 Jun 2020) | 5 lines
    KJB --> IVI
    
    Recorded result of commands processed by svn in /home/ivilab/repo_sync/ivilab_src_local/src/Make:
    Updating '.':
    U    BUILD_ENV
    U    init_scripts
    U    init_compile
    U    Makefile-2
    U    Makefile
    U    util/build
    U    util/Makefile
    U    Makefile-library
    U    Makefile-shared
    U    Makefile-program
    U    init_ua_hpc
    U    Makefile-include-xxx
    U    Makefile-makefile-dirs
    U    init_machine
    U    init_tacc_hpc
    D    scripts/kjb_svn_rm
    D    scripts/kjb_lock
    D    scripts/kjb_add_makefiles
    D    scripts/kjb_makedepend
    U    scripts/regress_program
    U    scripts/clean_global
    U    scripts/clean_cache
    U    scripts/get_local_include_dirs
    U    scripts/build_makefile_dirs
    U    scripts/echo_unless_quiet
    U    scripts/finish_executable
    U    scripts/build_sub_libs_needed
    U    scripts/copy_if_not_empty
    U    scripts/create_library
    U    scripts/build_test_depend_2
    U    scripts/build_test_depend_3
    U    scripts/export_machine_vars
    U    scripts/clean_build
    U    scripts/test_program
    U    scripts/echo_ld_path
    U    scripts/stream_prog_lib_obs
    U    scripts/clean_obj
    U    scripts/build_include_lines_2
    U    scripts/test_older_than
    D    scripts/test/test_kjb_lock
    D    scripts/test/test_kjb_lock_2
    A    scripts/test/test_ivi_lock
    A    scripts/test/test_ivi_lock_2
    U    scripts/clean_src
    U    scripts/build_makefile_libs_needed_2
    U    scripts/create_program
    U    scripts/update_include_line_file
    U    scripts/build-2
    A    scripts/ivi_lock
    U    scripts/build
    U    scripts/echo_machine
    U    scripts/stream_script_names
    A    scripts/ivi_makedepend
    U    scripts/make_src_dist
    U    scripts/build_makefile_libs_needed
    U    scripts/build_file_list_2
    U    scripts/build_incl_dot_h
    U    scripts/do_ctags
    U    scripts/define_build_aliases.sh
    U    scripts/test_program_2
    U    scripts/build_file_list
    U    scripts/self_update
    U    scripts/fix_incl
    U    scripts/get_link_target
    A    scripts/ivi_svn_rm
    U    scripts/Makefile-2
    U    scripts/stream_file_names
    U    scripts/Makefile
    U    scripts/copy_if_different
    A    scripts/ivi_add_makefiles
    U    scripts/echo_if_verbose
    U    scripts/init_compile.sh
    U    scripts/make_src_shadow_dir
    U    scripts/make_link
    U    scripts/build_include_lines
    U    scripts/clean_local
    U    scripts/build_makefile_dirs_2
    U    scripts/touch_if_exists
    U    Makefile-test
    U    build_with_cmake
    U    build_with_configure
    U    Makefile-defs
    U    build
    U    Makefile-2-xxx
    U    build_imported
    Updated to revision 25496.

--- Make/scripts/define_build_aliases.sh
+++ Make/scripts/define_build_aliases.sh
@@ -3,62 +3,62 @@
 # should be copied for use by bash as appropriate. Copying runs the risk of
 # missing out on updates. 
 
-# This script assumes that KJB_SRC_PATH points to a directory which has Make as a
+# This script assumes that IVI_SRC_PATH points to a directory which has Make as a
 # subirectory that has the init_compile script in it, and also scripts as a
 # sub-sub directory. (The standard layout in SVN of src/Make). Since the default
-# location of this script is ${KJB_SRC_PATH}/Make/scripts, it seems safe to assume
-# that KJB_SRC_PATH can be set as it is implicit in calling this script anyway. 
+# location of this script is ${IVI_SRC_PATH}/Make/scripts, it seems safe to assume
+# that IVI_SRC_PATH can be set as it is implicit in calling this script anyway. 
 
 # These should be updated in sync with the tcsh versions in ~kobus/.cshrc2. They are created
 # from the tcsh versions by simple replacements:
 #   s# '#='# 
-#   s#\$MACROS/init_compile_full#$KJB_SRC_PATH/Make/scripts/init_compile.sh#
+#   s#\$MACROS/init_compile_full#$IVI_SRC_PATH/Make/scripts/init_compile.sh#
 #   s#setenv  *\([^ ;'][^ ;']*\) #export \1=#g
 #   s#unsetenv  *\([^ ;'][^ ;']*\) *;#export \1=;#g
 #
-alias set_default='export KJB_CC=; export KJB_CXX=; export KJB_F77=; export KJB_LD=; export PRODUCTION=; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_default='export IVI_CC=; export IVI_CXX=; export IVI_F77=; export IVI_LD=; export PRODUCTION=; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
-alias set_gnu='if [[ -z "${FORCE_GCC_VERSION}" ]]; then suffix=""; else suffix="-${FORCE_GCC_VERSION}"; fi; export KJB_CC=gcc${suffix}; export KJB_CXX=g++${suffix}; export KJB_F77=gfortran${suffix}; export KJB_LD=${KJB_CC}${suffix}; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_gnu='if [[ -z "${FORCE_GCC_VERSION}" ]]; then suffix=""; else suffix="-${FORCE_GCC_VERSION}"; fi; export IVI_CC=gcc${suffix}; export IVI_CXX=g++${suffix}; export IVI_F77=gfortran${suffix}; export IVI_LD=${IVI_CC}${suffix}; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
 alias set_gcc='set_gnu'
 alias set_g++='set_gnu'
 
-alias set_clang='export KJB_CC=clang; export KJB_CXX=clang++; export KJB_F77=clang; export KJB_LD=${KJB_CC}; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_clang++='export KJB_CC=clang++; export KJB_CXX=clang++; export KJB_F77=clang; export KJB_LD=${KJB_CC}; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_icc='export KJB_CC=icc; export KJB_CXX=icpc; export KJB_F77=ifort; export KJB_LD=${KJB_CC}; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_clang='export IVI_CC=clang; export IVI_CXX=clang++; export IVI_F77=clang; export IVI_LD=${IVI_CC}; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_clang++='export IVI_CC=clang++; export IVI_CXX=clang++; export IVI_F77=clang; export IVI_LD=${IVI_CC}; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_icc='export IVI_CC=icc; export IVI_CXX=icpc; export IVI_F77=ifort; export IVI_LD=${IVI_CC}; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
 # Probably obsolete.
-# alias set_cc='export KJB_CC=cc; export KJB_CXX=CC; export KJB_LD=${KJB_CC}; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-# alias set_CC='export KJB_CC=CC; export KJB_CXX=CC; export KJB_LD=R{KJB_CC}; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+# alias set_cc='export IVI_CC=cc; export IVI_CXX=CC; export IVI_LD=${IVI_CC}; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+# alias set_CC='export IVI_CC=CC; export IVI_CXX=CC; export IVI_LD=R{IVI_CC}; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
-alias set_dev='export PRODUCTION=0; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_prod='export PRODUCTION=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_no_libs='export NO_LIBS=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_libs='export NO_LIBS=0; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_no_boost='export NO_BOOST=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_boost='export NO_BOOST=0; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_dev='export PRODUCTION=0; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_prod='export PRODUCTION=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_no_libs='export NO_LIBS=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_libs='export NO_LIBS=0; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_no_boost='export NO_BOOST=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_boost='export NO_BOOST=0; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
 # Asks for preferred CXX compiler on this system. 
-alias set_cxx='export KJB_PREFER_CXX=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_cxx='export IVI_PREFER_CXX=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 # Asks for preferred C compiler on this system. 
-alias set_c='export KJB_PREFER_CXX=0; if ($?KJB_CC != 0) unexport KJB_CC;=source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_c='export IVI_PREFER_CXX=0; if ($?IVI_CC != 0) unexport IVI_CC;=source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
-alias set_pch='export PREFER_PCH=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh' 
-alias set_no_pch='unexport PREFER_PCH;=source $KJB_SRC_PATH/Make/scripts/init_compile.sh' 
+alias set_pch='export PREFER_PCH=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh' 
+alias set_no_pch='unexport PREFER_PCH;=source $IVI_SRC_PATH/Make/scripts/init_compile.sh' 
 
-# alias set_after='export FORCE_NO_AFTER=0; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-# alias set_no_after='export FORCE_NO_AFTER=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+# alias set_after='export FORCE_NO_AFTER=0; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+# alias set_no_after='export FORCE_NO_AFTER=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
-alias set_no_opt='export FORCE_OPTIMIZE=0; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_opt='export FORCE_OPTIMIZE=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_no_test='export FORCE_TEST=0; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_test='export FORCE_TEST=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_debug='export FORCE_DEBUG=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_no_debug='export FORCE_DEBUG=0; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_no_opt='export FORCE_OPTIMIZE=0; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_opt='export FORCE_OPTIMIZE=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_no_test='export FORCE_TEST=0; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_test='export FORCE_TEST=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_debug='export FORCE_DEBUG=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_no_debug='export FORCE_DEBUG=0; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
-alias set_tma='export FORCE_TRACK_MEMORY_ALLOCATION=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_tma='export FORCE_TRACK_MEMORY_ALLOCATION=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
-alias set_mesa='export FORCE_OSMESA=1; export USE_OFFSCREEN_RENDERING=1; source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
-alias set_no_mesa='unexport FORCE_OSMESA;=unexport USE_OFFSCREEN_RENDERING;=source $KJB_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_mesa='export FORCE_OSMESA=1; export USE_OFFSCREEN_RENDERING=1; source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
+alias set_no_mesa='unexport FORCE_OSMESA;=unexport USE_OFFSCREEN_RENDERING;=source $IVI_SRC_PATH/Make/scripts/init_compile.sh'
 
 
--- Make/scripts/init_compile.sh
+++ Make/scripts/init_compile.sh
@@ -3,10 +3,10 @@
 ###############################################################################
 #
 # Identification:
-#    Provides the KJB build environment variables to bash users
+#    Provides the IVI build environment variables to bash users
 #
 # Description:
-#    This script provides the KJB build environment variables to bash users. It
+#    This script provides the IVI build environment variables to bash users. It
 #    is used by the bash versions of the build aliases (e.g., set_prod). It also
 #    adjust your PATH variable as the build changes.  Specifically, it also
 #    pre-pends the LD_OBJ_DIR to PATH. In this way, it is like the tcsh script
@@ -15,11 +15,11 @@
 #    This script must be sourced in bash (or its contents copied for use by
 #    bash). 
 #
-#    This script assumes that KJB_SRC_PATH points to a directory which has Make
+#    This script assumes that IVI_SRC_PATH points to a directory which has Make
 #    as a subirectory that has the init_compile script in it, and also scripts
 #    as a sub-sub directory. (The standard layout in SVN of src/Make). Since the
-#    default location of this script is ${KJB_SRC_PATH}/Make/scripts, it seems
-#    safe to assume that KJB_SRC_PATH can be set as it is implicit in calling
+#    default location of this script is ${IVI_SRC_PATH}/Make/scripts, it seems
+#    safe to assume that IVI_SRC_PATH can be set as it is implicit in calling
 #    this script anyway. 
 #
 ###############################################################################
@@ -28,18 +28,18 @@ export TMPDIR=/tmp
 
 # Make tcsh source init_compile. This might not be the most readible way, but it
 # works. 
-echo source ${KJB_SRC_PATH}/Make/init_compile | tcsh -s 
+echo source ${IVI_SRC_PATH}/Make/init_compile | tcsh -s 
 
 if [[ $? == 0 ]] 
 then
-    source ${TMPDIR}/${USER}/kjb_env.sh 
+    source ${TMPDIR}/${USER}/ivi_env.sh 
     if [[ $? != 0 ]] 
     then
-        echo "Sourcing ${TMPDIR}/${USER}/kjb_env.sh failed."
+        echo "Sourcing ${TMPDIR}/${USER}/ivi_env.sh failed."
         return 1
     fi 
 else 
-    echo "Sourcing  ${KJB_SRC_PATH}/Make/init_compile within tcsh failed."
+    echo "Sourcing  ${IVI_SRC_PATH}/Make/init_compile within tcsh failed."
     return 1
 fi 
 
