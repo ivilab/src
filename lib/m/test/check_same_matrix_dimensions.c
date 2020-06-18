549b117ffb97f93575cbb422ba4505ac27bc4798
ivilab-sync <sync@ivilab.org>
2020-06-18 01:32:13 -0700

    r25557 | abanerjee | 2020-06-18 01:30:36 -0700 (Thu, 18 Jun 2020) | 1 line
    Added non interactive test for check matrix dimensions
    r25558 | ivilab-sync | 2020-06-18 01:31:14 -0700 (Thu, 18 Jun 2020) | 1 line
    
    Adding /home/ivilab/repo_sync/local_staging/src/lib/./m/test/test_input/mat_error/.gitkeep because git cannot handle empty dirs.
    r25559 | ivilab-sync | 2020-06-18 01:31:15 -0700 (Thu, 18 Jun 2020) | 1 line
    
    Adding /home/ivilab/repo_sync/local_staging/src/lib/./m/test/test_input/mat_error/check_same_matrix_dimensions/.gitkeep because git cannot handle empty dirs.
    
    Recorded result of commands processed by svn in /home/ivilab/repo_sync/ivilab_src_local/src/lib:
    Updating '.':
    A    m/test/check_same_matrix_dimensions.c
    A    m/test/test_input/mat_error
    A    m/test/test_input/mat_error/.gitkeep
    A    m/test/test_input/mat_error/check_same_matrix_dimensions
    A    m/test/test_input/mat_error/check_same_matrix_dimensions/3x3_1.txt
    A    m/test/test_input/mat_error/check_same_matrix_dimensions/3x3_2.txt
    A    m/test/test_input/mat_error/check_same_matrix_dimensions/3x3_3.txt
    A    m/test/test_input/mat_error/check_same_matrix_dimensions/5x3.txt
    A    m/test/test_input/mat_error/check_same_matrix_dimensions/2x7.txt
    A    m/test/test_input/mat_error/check_same_matrix_dimensions/.gitkeep
    Updated to revision 25559.

---
+++ lib/m/test/check_same_matrix_dimensions.c
@@ -0,0 +1,82 @@
+
+/* $Id: check_same_matrix_dimensions.c 4723 2009-11-16 18:57:09Z kobus $ */
+/*
+ * Author: Aditya Banerjee
+ * Purpose: Checks the check_same_matrix_dimensions subroutine on various
+ * inputs non-interactively.
+ */
+
+#include "m/m_incl.h" 
+#define MAX_FILENAME_LEN 256
+
+/*
+ * This function sets the value of the first string argument equal to the
+ * name of the full file path to the second string argument. The full file
+ * path in this case means the necessary prefix is attached to find the
+ * input file in test_input/mat_error/check_same_matrix_dimensions/
+ */
+void set_file_path(char* matrix_file, char* filename){
+  char base_path[] = "test_input/mat_error/check_same_matrix_dimensions/";
+
+  strcpy(matrix_file, base_path);
+  strcat(matrix_file, filename);
+
+}
+
+/*
+ * The directive function that runs the tests.
+ */
+int main()
+{
+    Matrix* mp1 = NULL;
+    Matrix* mp2 = NULL;
+    char *matrix_file[MAX_FILENAME_LEN];
+    
+
+    /* Tests expected to pass */
+    
+    set_file_path(matrix_file, "3x3_1.txt");
+    EPETE(read_matrix(&mp1, matrix_file));  /* A 3x3 matrix */
+        
+    set_file_path(matrix_file, "3x3_2.txt");/* A different 3x3 matrix */
+    EPETE(read_matrix(&mp2, matrix_file));  
+   
+    /* Check for the exact same matrix */
+    EPE(check_same_matrix_dimensions(mp1, mp1, NULL));
+    EPE(check_same_matrix_dimensions(mp2, mp2, NULL));
+   
+    /* Check two different mxn matrices */
+    EPE(check_same_matrix_dimensions(mp1, mp2, NULL));
+    EPE(check_same_matrix_dimensions(mp2, mp1, NULL));
+
+    set_file_path(matrix_file, "3x3_3.txt");/* Another 3x3 matrix */
+    EPETE(read_matrix(&mp2, matrix_file));  
+
+    /* Check two mxn matrices but one of them has double digit values */
+    EPE(check_same_matrix_dimensions(mp1, mp2, NULL));
+    EPE(check_same_matrix_dimensions(mp2, mp1, NULL));
+
+
+   
+    /* Tests we expect to fail*/
+    
+    set_file_path(matrix_file, "2x7.txt");/* A 2x7 matrix */
+    EPETE(read_matrix(&mp1, matrix_file));  
+    
+    set_file_path(matrix_file, "5x3.txt");/* A 5x3 matrix */
+    EPETE(read_matrix(&mp2, matrix_file));  
+   
+
+    /* Check mxn matrix against pxq */
+    int retval = check_same_matrix_dimensions(mp1, mp2, NULL);
+    int retval2 = check_same_matrix_dimensions(mp2, mp1, NULL);
+    if (retval != ERROR || retval2 != ERROR){ 
+      printf("The test did not fail as expected\n");
+    }
+    
+    /* Cleanup */
+    free_matrix(mp1);
+    free_matrix(mp2);
+
+    return EXIT_SUCCESS; 
+}
